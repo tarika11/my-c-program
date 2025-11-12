c4bf231380fde7524d6cb074345a89dedf95fa53
tarika11 <tarikamurali07@gmail.com>
2025-11-12 22:31:07 +0530

    Add files via upload

---
+++ numneg.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int n;
+   printf("enter an integer:");
+   scanf("%d",&n);
+   if(n<0)
+   {
+       printf("you entered %d.\n",n);
+
+   }
+   printf("the if statement is easy.");
+    return 0;
+}
